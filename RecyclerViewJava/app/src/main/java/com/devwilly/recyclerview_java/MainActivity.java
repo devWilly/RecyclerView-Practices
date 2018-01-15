8388ccd11f355c42c87dee0bf0f6f917395bfa9e
Willy Chang <ms70831@gmail.com>
2018-01-15 11:28:14 +0800

    create recyclerview-java project

---
+++ RecyclerViewJava/app/src/main/java/com/devwilly/recyclerview_java/MainActivity.java
@@ -0,0 +1,14 @@
+package com.devwilly.recyclerview_java;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
