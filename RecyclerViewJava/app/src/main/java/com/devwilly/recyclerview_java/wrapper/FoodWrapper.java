1b90df22a860343115230b9d222e6457db9e5e9e
Willy Chang <ms70831@gmail.com>
2018-01-15 12:09:39 +0800

    create fruit and food data wrapper

---
+++ RecyclerViewJava/app/src/main/java/com/devwilly/recyclerview_java/wrapper/FoodWrapper.java
@@ -0,0 +1,41 @@
+package com.devwilly.recyclerview_java.wrapper;
+
+/**
+ * Created by Willy on 15/01/2018.
+ */
+
+public class FoodWrapper implements IEattingType {
+
+    enum FoodCookingMethod {
+        SAUTE, STEW, BROIL, BAKE, FRY, DEEPFRY
+    }
+
+    private String mName;
+    private String mPrice;
+    private FoodCookingMethod mCookingMethod;
+
+    public FoodWrapper(String name, String price, FoodCookingMethod cookingMethod) {
+        this.mName = name;
+        this.mPrice = price;
+        this.mCookingMethod = cookingMethod;
+    }
+
+    public FoodCookingMethod getCookingMethod() {
+        return mCookingMethod;
+    }
+
+    @Override
+    public String getName() {
+        return mName;
+    }
+
+    @Override
+    public String getPrice() {
+        return mPrice;
+    }
+
+    @Override
+    public int getItemType() {
+        return 0;
+    }
+}
---
+++ RecyclerViewJava/app/src/main/java/com/devwilly/recyclerview_java/wrapper/FruitWrapper.java
@@ -0,0 +1,31 @@
+package com.devwilly.recyclerview_java.wrapper;
+
+/**
+ * Created by Willy on 15/01/2018.
+ */
+
+public class FruitWrapper implements IEattingType {
+
+    private String mName;
+    private String mPrice;
+
+    public FruitWrapper(String name, String price) {
+        this.mName = name;
+        this.mPrice = price;
+    }
+
+    @Override
+    public String getName() {
+        return mName;
+    }
+
+    @Override
+    public String getPrice() {
+        return mPrice;
+    }
+
+    @Override
+    public int getItemType() {
+        return 0;
+    }
+}
