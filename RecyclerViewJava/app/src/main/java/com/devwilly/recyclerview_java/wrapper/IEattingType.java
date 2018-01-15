ed6571b8db06addfafbf76434dc5431297474bd3
Willy Chang <ms70831@gmail.com>
2018-01-15 12:09:20 +0800

    create item interface

---
+++ RecyclerViewJava/app/src/main/java/com/devwilly/recyclerview_java/wrapper/IEattingType.java
@@ -0,0 +1,14 @@
+package com.devwilly.recyclerview_java.wrapper;
+
+/**
+ * Created by Willy on 15/01/2018.
+ */
+
+public interface IEattingType {
+
+    String getName();
+
+    String getPrice();
+
+    int getItemType();
+}
