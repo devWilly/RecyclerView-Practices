506c40f8236df6a1be3e9c311d46d72c8878a43f
Willy Chang <ms70831@gmail.com>
2018-01-15 18:27:04 +0800

    implement default recyclerview button click event

--- RecyclerView-Kotlin/app/src/main/java/com/devwilly/recyclerviewkotlin/MainActivity.kt
+++ RecyclerView-Kotlin/app/src/main/java/com/devwilly/recyclerviewkotlin/MainActivity.kt
@@ -1,12 +1,22 @@
 package com.devwilly.recyclerviewkotlin
 
+import android.content.Intent
 import android.support.v7.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.Button
 
 class MainActivity : AppCompatActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        findViewById<Button>(R.id.default_recycler_view_btn).setOnClickListener { openActivity(DefaultRecyclerViewActivity::class.java) }
+    }
+
+    private fun openActivity(cls: Class<*>) {
+        val intent = Intent()
+        intent.setClass(this@MainActivity, cls)
+        startActivity(intent)
     }
 }
