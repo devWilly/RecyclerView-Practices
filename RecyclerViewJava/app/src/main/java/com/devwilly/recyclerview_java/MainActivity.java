7313cdbb1f2ca02dc6d16a7833914e57283afbe3
Willy Chang <ms70831@gmail.com>
2018-01-15 12:59:28 +0800

    implement default recyclerview button click event

--- RecyclerViewJava/app/src/main/java/com/devwilly/recyclerview_java/MainActivity.java
+++ RecyclerViewJava/app/src/main/java/com/devwilly/recyclerview_java/MainActivity.java
@@ -1,7 +1,9 @@
 package com.devwilly.recyclerview_java;
 
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
 
 
 public class MainActivity extends AppCompatActivity {
@@ -10,5 +12,18 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        findViewById(R.id.default_recycler_view_btn).setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                openActivity(DefaultRecyclerViewActivity.class);
+            }
+        });
+    }
+
+    private void openActivity(Class<? extends AppCompatActivity> cls) {
+        Intent intent = new Intent();
+        intent.setClass(MainActivity.this, cls);
+        startActivity(intent);
     }
 }
